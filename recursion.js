e0c06910daa67cbe9cfea670ffb2226ffd6a7eae
jingxia1219 <jingxia1219@gmail.com>
2018-10-17 14:26:13 -0700

    added exponent recursion

--- recursion.js
+++ recursion.js
@@ -6,3 +6,21 @@ function range(start, end) {
 }
 
 console.log(range(1, 4));
+
+function sumRec(arr) {
+    if (arr.length === 1) {
+        return arr[0];
+    }
+    return arr[0] + sumRec(arr.slice(1));
+}
+
+console.log(sumRec([1, 2, 3])); // tested and works
+
+function exponent(base, exp) {
+    if (exp === 1) {
+        return base;
+    }
+    return base * exponent(base, exp - 1);
+}
+
+console.log(exponent(2, 3)); //tested and works
