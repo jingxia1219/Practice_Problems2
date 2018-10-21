772761221892e70dfa2bb160788ae857543e7aef
jingxia1219 <jingxia1219@gmail.com>
2018-10-21 16:41:43 -0700

    added javascript let scope

--- javaScript_new.js
+++ javaScript_new.js
@@ -10,3 +10,13 @@ function Cat(name) {
   //variable defined that has greeting hi s4
 
   console.log(l2.greeting); //hi s2
+
+
+  var num = 0;
+
+for (let i =0l i<5; i++) {
+  num += i;
+}
+
+console.log(i); // undefined cos 
+// i is defined and only available in the for loop
