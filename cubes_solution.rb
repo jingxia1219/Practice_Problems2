e6bdfe8339003480414e7da6e094cd0fbfcacf7f
jingxia1219 <jingxia1219@gmail.com>
2018-10-22 13:59:08 -0700

    adfd

--- cubes_solution.rb
+++ cubes_solution.rb
@@ -4,9 +4,9 @@ def cubes_solution
     hash = {}
     result = {}
       a = 0 
-      while a < 13
+      while a < 1000
         b = 0 
-          while b < 13 
+          while b < 1000 
             hash[a**3+b**3] = [a,b]
             b+=1
           end 
@@ -15,29 +15,18 @@ def cubes_solution
       # p hash.values.include?([12,1])
       c = 0
       hash2 = {}
-      while c < 11
+      while c < 1000
         d = 0 
-          while d < 12
+          while d < 1000
           hash2[c**3 + d**3] = [c,d]
             if hash[c**3+d**3]
-            #  print 'found ya' unless hash[c**3+d**3].include?(c)
-            # print 'found ya' unless hash[c**3+d**3].include?(d)
-            #  p d 
-                
                 result[hash[c**3+d**3]+[c,d]] = true
             end 
             d+=1
           end 
           c+=1
       end 
-      # p result
-    
-      # taxi_cabs = result.select { |el| el[0..1] = [12,1]}
-      # p taxi_cabs
-      # p hash2.values.uniq.include?([10,9])
-      # p result.include
-      p result.keys.include?([12,1,10,9])
-      p result.select { |el| el.uniq.length > 2 }
+     p result.keys.select { |el| el.uniq.length > 2}
     end 
-    
+    # time compleixty O(n^2)
     cubes_solution
