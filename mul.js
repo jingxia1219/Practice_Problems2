05f66e558752e47b5fb9d6a8a1584a682e9a1df1
jingxia1219 <jingxia1219@gmail.com>
2018-10-22 18:49:50 -0700

    adfdf

---
+++ mul.js
@@ -0,0 +1,14 @@
+
+function mul(x) {
+    return function(y) {
+        return function(z) {
+            return (x*y*z);
+        }
+    }
+    // }
+}
+
+
+console.log(mul(2)(3)(4)); // output : 24 
+console.log(mul(4)(3)(4)); // output : 48
+
