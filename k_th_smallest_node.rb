2185864a9d60745e5e3ce6b4b9dedf90b0ca8b5e
jingxia1219 <jingxia1219@gmail.com>
2018-10-24 17:40:33 -0700

    add

--- k_th_smallest_node.rb
+++ k_th_smallest_node.rb
@@ -2,21 +2,37 @@
 
 class Node  
 attr_accessor :val, :left, :right 
-    def initialize(val,left = nil, right = nil)
+    def initialize(val,left = nil, right = nil,parent = nil)
         @val = val
         @left= left
         @right = right 
+        @parent = parent 
     end 
 
     def insert(node)
+
     end 
 
     def delete(node)
+
     end 
 end 
 
-def find_kth_smallest(root)
-
+def find_kth_smallest(root,k)
+    smallest = find_smallest(root)
+    curr_node = root 
+    while k>0
+    if curr_node.right 
+        k-=1 
+        if k == 0
+            return curr_node.right 
+        end 
+        curr_node.right = nil 
+    else 
+        curr_node = curr_node.parent
+        k-=1 
+    end 
+    curr_node
 end 
 
 def find_smallest(root)
