998a163d4ea682a3ae4935f4ae563ae484ce9edb
jingxia1219 <jingxia1219@gmail.com>
2018-10-27 19:31:05 -0700

    adsdsf

--- bst_checker.rb
+++ bst_checker.rb
@@ -1,3 +1,4 @@
+# write a method that determines if a Binary Tree is super balanced, meaning if the shortest branch is at most 1 depth of compared to the longest branch 
 class Node 
     attr_accessor :val, :left, :right
       def initialize(val, left =nil,right=nil)
@@ -36,10 +37,70 @@ class Node
     end 
     
     
+    def binary_search_tree?(root)
+      # start at the root, with an arbitrarily low lower bound
+      # and an arbitrarily high upper bound
+      node_and_bounds_stack = []
+      node_and_bounds_stack.push([root, -Float::INFINITY, Float::INFINITY])
+    
+      # depth-first traversal
+      until node_and_bounds_stack.empty?
+        node, lower_bound, upper_bound = node_and_bounds_stack.pop
+    
+        # if this node is invalid, we return false right away
+         if (node.val <= lower_bound) || (node.val >= upper_bound)
+         print 'node.val :'
+         p node.val 
+         print 'lower_bound: '
+         p lower_bound
+         print 'upper_bound :'
+         p upper_bound
+         return false
+         end 
+        if node.left
+          # this node must be less than the current node
+          node_and_bounds_stack.push([node.left, lower_bound, node.val])
+        end
+        if node.right
+          # this node must be greater than the current node
+          node_and_bounds_stack.push([node.right, node.val, upper_bound])
+        end
+      end
+    
+      # if none of the nodes were invalid, return true
+      # (at this point we have checked all nodes)
+      true
+    end
+    
+    node8 = Node.new(8)
+    node6= Node.new(6,node8)
+    node4 = Node.new(4,node6)
+    node3 = Node.new(3)
+    node2 = Node.new(2, node4, node3)
+    node7 = Node.new(7)
+    node3 = Node.new(3,node7)
+    node1 = Node.new(1,node2,node3)
+    
+    # super_balanced?(node1)
+    
+    
+    def dfs(root,val)
+      nodes =[]
+      nodes << root
+      until nodes.empty? 
+        node = nodes.pop 
+        return node if node.val == val 
+      nodes << node.left if node.left 
+      nodes << node.right if node.right 
+      end 
+      nil
+    end 
+    
+    # dfs(node1,3)
+    
+    # write a method to check that a binary tree is a valid binary search tree.Check for left is smaller than parent and right is greater than parent 
+    
     def bst?(root)
-        # Time complexity is O(n), for valid BST, we have to check all nodes
-        # Space complexity is O(n), greatest_ancestor and smallest O(1)
-        # nodes O(d) d being the depth/level
       nodes = []
       greatest_ancestor = nil
       smallest_ancestor = nil
@@ -48,22 +109,12 @@ class Node
       until nodes.empty?
        node, greatest_ancestor,smallest_ancestor = nodes.pop
         if (greatest_ancestor && node.val > greatest_ancestor )|| (smallest_ancestor && node.val < smallest_ancestor)
-            # check that all nodes in left subtree is less than root and all nodes 
-            #  in the right subtree is greater than the root on top of 
-            #  all left is less than direct parent and right is greater than direct parent
-
-            # e.g      root=50 
-            #          /      \
-            #        30       80
-            #       /  \     /   \
-            #     20   *60  70   90
-            # every right is greater than its direct parent, but 60 is not smaller than 
-            # its great parent
+       
         return false 
         end 
         
         if node.left 
-           if node.left.val > node.val #checks the direct parent node relationship
+           if node.left.val > node.val 
              p 'line83'
                return false
            end 
@@ -71,11 +122,6 @@ class Node
         # return false if left.val > node.val 
         if greatest_ancestor.nil? || node.val > greatest_ancestor
          nodes << [node.left, node.val,smallest_ancestor]
-        else 
-            nodes << [node.left, greatest_ancestor,smallest_ancestor]
-        #  DON'T use global variable greatest ancestor here as it will affect
-        #  the other side's subtree, but directly use the value in the greatest
-        #  ancestor's place will only pass the value down to its own line
          end 
           # greatest_ancestor = nil 
         end 
@@ -85,70 +131,78 @@ class Node
               p 'line 94'
                 return false 
               end 
-             if smallest_ancestor.nil? || node.val < smallest_ancestor
+             if smallest_ancestor.nil? || 
+              node.val < smallest_ancestor
+    
               nodes << [node.right, greatest_ancestor,node.val ]
-             else 
-                nodes << [node.left, greatest_ancestor,smallest_ancestor]
               end 
               # smallest_ancestor = nil 
           end 
-        #   p nodes
-
+          p '------'
+          p nodes
+          p 's-----'
         end 
           true 
+          
       end 
     
-    def binary_search_tree?(root)
-  # start at the root, with an arbitrarily low lower bound
-  # and an arbitrarily high upper bound
-  node_and_bounds_stack = []
-  node_and_bounds_stack.push([root, -Float::INFINITY, Float::INFINITY])
-
-  # depth-first traversal
-  until node_and_bounds_stack.empty?
-    node, greatest_ancestor, smallest_ancestor = node_and_bounds_stack.pop
-
-    # if this node is invalid, we return false right away
-    return false if (node.value <= greatest_ancestor) || (node.value >= smallest_ancestor)
-
-    if node.left
-      # this node must be less than the current node
-      node_and_bounds_stack.push([node.left, greatest_ancestor, node.value])
-    end
-    if node.right
-      # this node must be greater than the current node
-      node_and_bounds_stack.push([node.right, node.value, smallest_ancestor])
-    end
-  end
-
-  # if none of the nodes were invalid, return true
-  # (at this point we have checked all nodes)
-  true
-end
+    
     
     node20 = Node.new(20)
-    node60 = Node.new(40)
+    node60 = Node.new(60)
     node30 = Node.new(30, node20,node60)
     node70 = Node.new(70)
     node90 = Node.new(90)
     node80 = Node.new(80,node70,node90)
     node50 = Node.new(50,node30, node80)
     
-     bst?(node50)
-
+    # p bst?(node50)
+    # p binary_search_tree?(node50)
     node_left = Node.new(1)
-node_right3 = Node.new(5)
-node_right1 = Node.new(4,nil,node_right3)
-node_left2 = Node.new(2.5)
-node_right2 = Node.new(3,node_left2,node_right1)
-root = Node.new(2,node_left, node_right2)
-
-def second_largest(root)
-   min = root
-  while min.right
-    parent = min
-    min = min.right
-  end 
-  parent 
-end 
-p second_largest(root)
+    node_right3 = Node.new(5)
+    node_right1 = Node.new(4,nil,node_right3)
+    node_left2 = Node.new(2.5)
+    node_right2 = Node.new(3,node_left2,node_right1)
+    root = Node.new(2,node_left, node_right2)
+    
+    
+    node_9 = Node.new(9)
+    node_11 = Node.new(11)
+    node_10 = Node.new(10,node_9, node_11)
+    node_12 = Node.new(12,node_10)
+    node_7 = Node.new(7)
+    node_8 = Node.new(8,node_7,node_12)
+    node_1 = Node.new(1)
+    node_4 = Node.new(4)
+    node_3 = Node.new(3, node_1, node_4)
+    node_5 = Node.new(5, node_3,node_8)
+    
+    def second_largest(root)
+       max = root
+       if root.right.nil?
+       raise 'has to be at least two nodes to find the 2nd largest' if root.left.nil?
+        return find_max(root.left)  
+       end 
+      while max.right
+      # O(n) when there's only right subtree
+        parent = max
+        max = max.right
+      end 
+       p max
+      p max.left.nil?
+      if max.left.nil? 
+        return parent 
+      else 
+       find_max(max.left)
+      end 
+      
+    end 
+    
+    def find_max(node)
+     sub_max = node
+        while sub_max.right
+          sub_max = sub_max.right
+        end 
+        return sub_max
+    end 
+    p second_largest(node_3)
