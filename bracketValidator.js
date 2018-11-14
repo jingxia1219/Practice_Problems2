3286861afd9aea8f9484c1d288afa850c052ad9d
jingxia1219 <jingxia1219@gmail.com>
2018-11-13 19:11:36 -0800

    sfs

---
+++ bracketValidator.js
@@ -0,0 +1,52 @@
+// "{ [ ] ( ) }" should return true
+// "{ [ ( ] ) }" should return false
+// "{ [ }" should return false
+
+function bracketValidator(str) {
+    var obj = new Object();
+    obj['curlyOpener'] = obj['bracketOpener'] = obj['parenOpener'] = obj["curlyCloser"] = obj["bracketCloser"] = obj["parenCloser"] = 0;
+    var lastOpen; 
+  
+    for (let i = 0; i < str.length; i ++) {
+       if (str[i] === '{') {
+         obj["curlyOpener"]++;
+         lastOpen = "curly";
+      }
+      if (str[i] === '}') {
+         obj["curlyCloser"]++;
+            if (lastOpen !== "curly") {
+           return false;
+         }
+        lastOpen = null;
+      }
+      if (str[i] === '[') {
+         obj["bracketOpener"]++;
+         lastOpen = 'bracket';
+      }
+       if (str[i] === ']') {
+         obj["bracketCloser"]++;
+            if (lastOpen !== "bracket") {
+           return false;
+         }
+         lastOpen = null;
+      }
+      if (str[i] === '(') {
+         obj["parenOpener"]++;
+         lastOpen = 'paren'
+      }
+      if (str[i] === ')') {
+         obj["parenCloser"]++;
+         if (lastOpen !== "paren") {
+           return false;
+         }
+         lastOpen = null;
+      }
+    }
+      if (obj['curlyOpener'] !== obj['curlyCloser'] || obj['parenOpener'] !== obj['parenCloser'] || obj['bracketOpener']!== obj['bracketCloser']) {
+        return false;
+      }
+      return true;
+  }
+  
+   bracketValidator("(){[}]")
+  
