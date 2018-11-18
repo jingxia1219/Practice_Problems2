d5d3ad0713b873d1d8f6fc82aeaca33c5625d628
jingxia1219 <jingxia1219@gmail.com>
2018-11-17 21:37:59 -0800

    added rec_reverse.rb

--- recursively_reverse_linkelist.rb
+++ recursively_reverse_linkelist.rb
@@ -7,9 +7,7 @@ class Node
       end 
     end 
     
-    # node3 = Node.new(3)
-    node2 = Node.new(2)
-    node1 = Node.new(1,node2) #head
+    
     # p node1
     # 1 => 2 => 3 => nil 
     # return 3 => 2 =>1 => nil
@@ -25,20 +23,28 @@ class Node
     #     head.next_node = nil 
     #  end
 
+    # node3 = Node.new(3)
+    node2 = Node.new(2)
+    node1 = Node.new(1,node2) #head
 
-    def reverse_linkedlist(head)
-      # 1 => nil 
-      #1. return the passed in head if there's no next_node (base case)
-      return head if head.next_node.nil? 
-    #   head = 2
-    #   2 => nil returned
-      # 1 => 2 => nil 
-      head2 = head.dup 
-      reverse_linkedlist(head.next_node).next_node = head2 
-      # 2.next_node = 1 
-      p "head: #{head.val}"
-      #2. 
-    end 
-    
+   
+    def reverse_linkedlist(head,prev =nil)
+        return prev if head.nil? 
+        next_n = head.next_node
+        
+        head.next_node = prev 
+        # p "head_after: #{head.next_node.val}" 
+        print 'prev: '
+        p prev
+        p '-----'
+        print 'head_after'
+        p head
+            p '----------'
+        print "next_n: " 
+        p next_n
+            p '----------------------'
+        reverse_linkedlist(next_n,head)
+      end 
+ 
     
      p reverse_linkedlist(node1)
