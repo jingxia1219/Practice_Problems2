1b929be451620fb8de9b284a9043f11428924b31
jingxia1219 <jingxia1219@gmail.com>
2018-11-19 18:38:44 -0800

    ssfd

--- word_cloud.dictionary.rb
+++ word_cloud.dictionary.rb
@@ -28,3 +28,46 @@ def word_cloud(str)
   p word_cloud("After beating the eggs, Dana read the next step:
   Add milk and eggs, then add flour and sugar.")
   p word_cloud("Alright. I'll write my own examples, examples.")
+
+#   Second solution:
+# ***************************************************************
+
+  def word_cloud(str)
+    # create a hash
+      hash = Hash.new {0}
+      # 1. split str into words 
+      own_split(str).each do |word|
+        hash[word] += 1
+      end 
+        hash
+        # a. case insensitive
+        # b. ignore punction
+    
+    end 
+    
+     def own_split(str)
+    #  takes in a string, return words without punctuations and all lower case 
+    
+    lower_case = ("a".."z").to_a
+    upper_case = ("A".."Z").to_a
+    letters = lower_case + upper_case
+    words = []
+    substring = ""
+      str.length.times do |i|
+        if letters.include?(str[i])
+           substring+= str[i].downcase
+          #  p substring
+        else 
+          unless substring.length == 0
+            words << substring
+            substring = "" 
+          end 
+        end 
+      end 
+      words
+     end 
+    
+     p Time.now
+     p word_cloud(  "After beating the eggs, Dana read the next step:")
+    
+    p Time.now
