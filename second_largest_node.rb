d0ede2faa957e264f5eadd52c421b6277dc55edf
jingxia1219 <jingxia1219@gmail.com>
2018-11-20 17:23:14 -0800

    sdf

--- second_largest_node.rb
+++ second_largest_node.rb
@@ -16,12 +16,27 @@ class BST
   node7 = BST.new(7)
   right1 = BST.new(6,node5,node7)
   
-  root = BST.new(3,left1,right1)
+  root = BST.new(3,left1)
   
   # p root
   # second largest node should have val 6
   
+  def largest_node(root)
+    largest = root
+    until largest.right.nil?
+      largest = largest.right
+    end 
+    largest
+  end 
+  
   def second_largest_node(root)
+    if root.right.nil? 
+      if root.left.nil?
+        raise ' need at least 2 nodes'
+      else 
+        return largest_node(root.left)
+      end 
+    end 
     largest = root
     parent = nil
     until largest.right.nil?
